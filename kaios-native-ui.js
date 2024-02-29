c061ada12b20e97c6026707f0355aa23e5d5b33b
W4IT <w4it.dev.business@gmail.com>
2024-02-29 19:52:57 +0100

    ALMOST done Added translations (de,en,es,ru, zh) Changed app icon Gotta re-add download&share icons compressed code (JSONs, CSS and some JS).

--- kaios-native-ui.js
+++ kaios-native-ui.js
@@ -1,17 +1,1 @@
-let classesWithColoredParents =
-  /checkbox-container__input|radio-container__input|input-container__input|textarea-container__textarea|slider-container__slider/g;
-let classesWithCheckboxes = /radio-container__input|checkbox-container__input/g
-
-const callFunction = (callback, e) => {
-  let element = e.target;
-  //if element has any of those classes in regex then its parent will change class.
-  if (element.className && element.className.match(classesWithColoredParents))
-    callback(element.parentElement);
-};
-
-const blur = (element) => element.classList.remove("selected");
-
-const focus = (element) => element.classList.add("selected");
-
-window.addEventListener("focus", (e) => callFunction(focus, e), true);
-window.addEventListener("blur", (e) => callFunction(blur, e), true);
+let classesWithColoredParents=/checkbox-container__input|radio-container__input|input-container__input|textarea-container__textarea|slider-container__slider/g,classesWithCheckboxes=/radio-container__input|checkbox-container__input/g;const callFunction=(e,t)=>{let n=t.target;n.className&&n.className.match(classesWithColoredParents)&&e(n.parentElement)},blur=e=>e.classList.remove("selected"),focus=e=>e.classList.add("selected");window.addEventListener("focus",e=>callFunction(focus,e),!0),window.addEventListener("blur",e=>callFunction(blur,e),!0);
